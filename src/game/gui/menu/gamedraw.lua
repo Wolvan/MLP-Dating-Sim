e01b99acb784ae2ae6add6a492a98d54d952b1f4
Wolvan <wolvan1@gmail.com>
2015-02-04 09:45:12 +0100

    Added update functions to the currently created menu classes Changed the update and draw code from main menu and split the code in the respective classes

--- src/game/gui/menu/gamedraw.lua
+++ src/game/gui/menu/gamedraw.lua
@@ -1,3 +1,4 @@
+local t = 0
 Menus.Game = Menu:new({
 	draw = function(self)
         image = loadImg(DrawVars.BackgroundImg)
@@ -6,11 +7,14 @@ Menus.Game = Menu:new({
 		love.graphics.draw(image,50,100)
 		image = loadImg(DrawVars.RightCharImg)
 		love.graphics.draw(image,720,100)
-		image = loadImg(DrawVars.GameRoundedRect)
+		image = loadImg("round_rectangle.png")
 		love.graphics.draw(image,0,520)
 		image = loadImg(DrawVars.GameFakeMoney)
 		love.graphics.draw(image,1050,0)
 		love.graphics.print("Daisy: Wow that's a lot of cash!", 30, 550)
 		love.graphics.print("Anon: It's all from pre-orders on this game.", 30, 580)
-    end
+    end,
+	update = function(self, dt)
+		-- Do Game Logic here
+	end
 })
--- src/game/gui/menu/mainmenu.lua
+++ src/game/gui/menu/mainmenu.lua
@@ -1,28 +1,14 @@
 Menus.MainMenu = Menu:new({
     draw = function(self)
-		Variables.Main.CurrentMenu = "MainMenu"
-
-		function love.update(dt)
-		  if Variables.Main.CurrentMenu == "MainMenu" then
-		    if love.keyboard.isDown("return", "enter") then
-		      Variables.Main.CurrentMenu = "Game"
-		    end
-		  else
-		    -- do your game logic here
-		  end
-		end
-
-		function love.draw()
-		  if Variables.Main.CurrentMenu == "MainMenu" then
-	        image = loadImg(DrawVars.MainMenuBGImg)
-			love.graphics.draw(image,0,0)
-			image = loadImg(DrawVars.MainMenuLogoImg)
-			love.graphics.draw(image,400,50)
-		  else
-		    -- draw your game here
-		    Menus[Variables.Main.CurrentMenu].draw();
-		  end
-		end
+		image = loadImg("Ponyville_Main_Menu.png")
+		love.graphics.draw(image,0,0)
+		image = loadImg("mlp_logo_Editted.png")
+		love.graphics.draw(image,400,50)
         love.graphics.print("Welcome to the Main Menu", 10, 10)
-    end
+    end,
+	update = function(self, dt)
+		if love.keyboard.isDown("return", "enter") then
+			Variables.Main.CurrentMenu = "Game"
+		end
+	end
 })
