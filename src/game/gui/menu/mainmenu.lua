0e13f50bec9ab5aa5d8f2d25ea365eb9d2dbfecf
KodexMonkey <demondude57@GrimGrimoire.local>
2015-02-03 22:15:25 -0500

    Added Images, Tested out Drawing, Etc.
    Just testing out LUA and making it look good. None of this is final but
    just testing some things out in the design. Couldn’t figure out fonts
    though, I’ll try it out later but I did put the Equestria font in there
    anyway.

--- src/game/gui/menu/mainmenu.lua
+++ src/game/gui/menu/mainmenu.lua
@@ -1,5 +1,28 @@
 Menus.MainMenu = Menu:new({
     draw = function(self)
+		Variables.Main.CurrentMenu = "MainMenu"
+
+		function love.update(dt)
+		  if Variables.Main.CurrentMenu == "MainMenu" then
+		    if love.keyboard.isDown("return", "enter") then
+		      Variables.Main.CurrentMenu = "Game"
+		    end
+		  else
+		    -- do your game logic here
+		  end
+		end
+
+		function love.draw()
+		  if Variables.Main.CurrentMenu == "MainMenu" then
+	        image = loadImg(DrawVars.MainMenuBGImg)
+			love.graphics.draw(image,0,0)
+			image = loadImg(DrawVars.MainMenuLogoImg)
+			love.graphics.draw(image,400,50)
+		  else
+		    -- draw your game here
+		    Menus[Variables.Main.CurrentMenu].draw();
+		  end
+		end
         love.graphics.print("Welcome to the Main Menu", 10, 10)
     end
 })
--- src/game/objects/gamevars/mainvarlist.lua
+++ src/game/objects/gamevars/mainvarlist.lua
@@ -1,6 +1,6 @@
 Variables = {
     Main = {
-        CurrentMenu = "Game",
+        CurrentMenu = "MainMenu",
         ActiveCamera = "default",
         LoadedImg = {},
         LoadedSounds = {},
@@ -9,7 +9,17 @@ Variables = {
 }
 
 DrawVars = {
-	BackgroundImg = "Texture.png",
-	LeftCharImg = "Texture.png",
-	RightCharImg = "Texture.png"
+	BackgroundImg = "empty_Ponyville_street_illuminati.png",
+	LeftCharImg = "fake_daisy_vector.png",
+	RightCharImg = "anon_pony_by_aginpro.png",
+    MainMenuBGImg = "Ponyville_Main_Menu.png",
+    MainMenuLogoImg = "mlp_logo_Editted.png",
+    GameRoundedRect = "round_rectangle.png",
+    GameFakeMoney = "dat_money_doe.png"
+}
+-- KodexMonkey's Testing Variables
+TestVars = {
+    ChatBubble = "Default Text";
+    x = 100;
+    y = 100;
 }
